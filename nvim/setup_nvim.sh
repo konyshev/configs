489365d6b5a80ac5a94f1eddcf4df40d0dffba31
Enygma <y.konyshev@gmail.com>
2019-04-30 11:47:12 +0200

    changed curl for vim plug installation

--- nvim/setup_nvim.sh
+++ nvim/setup_nvim.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
 if ! [ -x "$(command -v nvim)" ]; then
-    echo 'Can not find NeoVim' >&2
-    echo 'Installation of NeoVim and VimPlug' >&2
+    echo '--> Can not find NeoVim' >&2
+    echo '--> Installation of NeoVim and VimPlug' >&2
     sudo add-apt-repository ppa:neovim-ppa/stable
     sudo apt-get update
     sudo apt-get install neovim
     
-    CURL='/usr/bin/curl'
-    CURLARGS="-fLo --create-dirs"
-    LOCALSTORAGE= "~/.local/share/nvim/site/autoload/plug.vim"
-    RVMHTTP="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
-
-    # you can store the result in a variable
-    $CURL $CURLARGS $LOCALSTORAGE $RVMHTTP
+    VIM_PLUG_CMD='curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+    $VIM_PLUG_CMD
 
 fi
 
