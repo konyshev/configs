b107f500ed229fe60df703a15265df08f3d88dd2
Yaroslav Konyshev <y.konyshev@gmail.com>
2020-02-21 20:59:20 +0100

    changed theme for vifm

--- config-collector.sh
+++ config-collector.sh
@@ -1,2 +1,5 @@
 cp ~/.config/nvim/init.vim ./nvim/init.vim
-cp ~/.config/i3/config ./i3wm/config
+cp -r ~/.config/i3/config ./i3wm/config
+cp -r ~/.config/yk ./
+cp -r ~/.config/vifm/colors ./vifm
+cp ~/.config/vifm/vifmrc ./vifm
